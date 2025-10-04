bf972143592b320036a86ffc7409d80aa7724058
vijayguhan10 <vijayguhan10@gmail.com>
2025-10-04 18:43:18 +0530

    changed the minimal version

--- cricket/src/App.jsx
+++ cricket/src/App.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 import Initialrouter from "./router/Initialrouter";
 import Footer from "./components/Footer";
 import Navigation from "./components/Header";
+
 function App() {
+  const location = useLocation();
+
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div className="font-poppins">
       <Navigation />
-      <Initialrouter />
+      <div className="pt-16 lg:pt-20 bg-black">
+        <Initialrouter />
+      </div>
       <Footer />
     </div>
   );
